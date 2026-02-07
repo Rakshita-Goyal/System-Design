a753a4a4051c691d1ccb4f336e79370bbe84f006
Rakshita Goyal <goyal31rakshita@gmail.com>
2026-02-07 08:18:43 +0530

    anti pattern

---
+++ lld/AntiPattern.java
@@ -0,0 +1,16 @@
+anti:Pattern
+God object:one class with many functions ,orchestration thats why ,use delegation 
+spagetti code:complicated code: dont know where start or end ,tighly coupled and prone to errors 
+hard code :can change the value , use only when know that it wont change 
+gold plating (over engineering) all patterns, do for cases that wont even come in real life 
+dry(don not repeat yourself )if logic changes,change everywhere ,seprate it only
+constructor overloading : use builder design 
+overuse of getter /setters 
+premature optimiization : make it work first then make it fast 
+overuse of inheritance : strategy pattern,bridge ,visitor pattern 
+
+Null object pattern :
+avoid null checks
+replace conditionals with polymorphism 
+with concrete methods have null objects also
+ 
